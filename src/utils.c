d5e71ddad0663bbd5aa46ae6a1d007d25556089c
Quentin Dassance <quentin.dassance@etu.estia.fr>
2025-06-16 20:12:07 +0200

    feature #20 finie

--- src/utils.c
+++ src/utils.c
@@ -6,28 +6,22 @@
 pixelRGB * get_pixel( unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y ) {
 
     if ((x >= height) || (y >= width)) {
-
         fprintf(stderr, "Erreur : image trop petite pour aller chercher le pixel demandé\n");
         return NULL;
-
     }
 
     else if (data == NULL) {
-
         fprintf(stderr, "Erreur : données image nulles\n");
         return NULL;
-
     }
 
     else {
-
         return (pixelRGB *) &data[(y * width + x) * n] ;
-
     }
 
 }
 
-void print_pixel( char *filename, int x, int y ) {
+void print_pixel(char *filename, int x, int y) {
 
     int width, height, channels;
     unsigned char *pixelArray;
@@ -36,10 +30,11 @@ void print_pixel( char *filename, int x, int y ) {
 
     if (result) {
         pixelRGB * pixel_recherche = get_pixel(pixelArray, width, height, channels, x, y);
-        printf("print_pixel (%d, %d): %d, %d, %d\n", x, y, pixel_recherche->R, pixel_recherche->G, pixel_recherche->B);
+        if (pixel_recherche != NULL) {
+            printf("print_pixel (%d, %d): %d, %d, %d\n", x, y, pixel_recherche->R, pixel_recherche->G, pixel_recherche->B);
+        } else {
+            fprintf(stderr, "Erreur : coordonnées pixel (%d, %d) invalides.\n", x, y);
+        }
         free(pixelArray);
-    } else {
-        fprintf(stderr, "Erreur : impossible de lire l'image %s\n", filename);
-    }    
-
+    }
 }
