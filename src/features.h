53580d25a30d378d02fba3418b9720211d3a2d47
Quentin Dassance <quentin.dassance@etu.estia.fr>
2025-06-18 17:17:59 +0200

    esthétique

--- src/features.h
+++ src/features.h
@@ -3,13 +3,16 @@
 
 
 void helloWorld();
+
 void dimension(char *source_path);
 void first_pixel (char *source_path);
 void tenth_pixel (char *source_path);
 void second_line(char *source_path);
+
 void max_pixel(char *source_path);
+void min_pixel(char *source_path);
+
 void max_component(char *source_path, char component);
 void min_component(char *source_path, char component);
-void min_pixel(char *source_path);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -58,6 +58,10 @@ int main(int argc, char **argv) {
     max_pixel(configuration.filenames[0]);
   }
 
+  if (strncmp(configuration.command, "min_pixel", 9) == 0) {
+    min_pixel(configuration.filenames[0]);
+  }
+
   if ( strncmp(configuration.command, "max_component", 13) == 0 ) {
     char component = configuration.arguments[0][0];
     max_component(configuration.filenames[0], component); 
@@ -66,13 +70,8 @@ int main(int argc, char **argv) {
   if ( strncmp(configuration.command, "min_component", 13) == 0 ) {
     char component = configuration.arguments[0][0];
     min_component(configuration.filenames[0], component); 
-  }
-
-  if (strncmp(configuration.command, "min_pixel", 9) == 0) {
-    min_pixel(configuration.filenames[0]);
-  }
+  }  
 
   return 0;
-
   
 }
