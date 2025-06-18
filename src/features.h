ad6da30f906b730acfa9d26f6c5dfa6b8de80f60
matthis duvert <matthis.duvert@etu.estia.fr>
2025-06-18 12:25:48 +0200

    maxcomponent_18

--- src/features.h
+++ src/features.h
@@ -8,5 +8,6 @@ void first_pixel (char *source_path);
 void tenth_pixel (char *source_path);
 void second_line(char *source_path);
 void max_pixel(char *source_path);
+void max_component(char *source_path, char component);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -54,9 +54,13 @@ int main(int argc, char **argv) {
     max_pixel(configuration.filenames[0]);
   }
   
-  if ( strncmp( configuration.command, "first_pixel", 11 ) == 0 ) {
-    /* first_pixel() function is defined in feature.h and implemented in feature.c */
-    first_pixel( configuration.filenames[0] );
+  if (strncmp(configuration.command, "first_pixel", 11) == 0) {
+    first_pixel(configuration.filenames[0]);
+  }
+
+  if ( strncmp(configuration.command, "max_component", 13) == 0 ) {
+    char component = configuration.arguments[0][0];
+    max_component(configuration.filenames[0], component); 
   }
 
   return 0;
